9a30caab3c4b129b4d398672dbc2fe2d6591c4e2
douae <d.amzir@karizma-conseil.com>
2020-07-02 18:30:41 +0100

    T4370 - Remarques : Recette Interne Référencement/Evaluation fournisseurs

--- kzm_referencement_v/models/ref.py
+++ kzm_referencement_v/models/ref.py
@@ -8,6 +8,6 @@ class Ref(models.Model):
     _description = "Referencement informations"
 
     ref_date = fields.Date("Demand date", default=fields.Date.today())
-    ref_val_date = fields.Date("Validation date")
+    ref_val_date = fields.Date("Validation date",default=fields.Date.today())
     ref_asked_by = fields.Many2one('res.users', string="Requested by", default=lambda self: self.env.uid)
-    ref_responsible = fields.Many2one('res.users', string="Responsible")
+    ref_responsible = fields.Many2one('res.users', string="Responsible", default=lambda self: self.env.uid)
