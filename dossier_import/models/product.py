5d75a4296cc2ee999431fb07e4eaacffc2185845
douae <d.amzir@karizma-conseil.com>
2020-07-06 14:40:49 +0100

    T4382 - Remarques : Recette Interne Budget/Dossier import

--- dossier_import/models/product.py
+++ dossier_import/models/product.py
@@ -3,20 +3,20 @@
 from odoo import api, fields, models
 
 SPLIT_METHOD = [
-    ('equal', 'Egal'),
-    ('by_quantity', 'Par Quantité'),
-    ('by_current_cost_price', 'Par prix actuel'),
-    ('by_weight', 'Par Poids'),
-    ('by_volume', 'Par Volume'),
+    ('equal', 'Equal'),
+    ('by_quantity', 'by quantity'),
+    ('by_current_cost_price', 'By cirrent price'),
+    ('by_weight', 'By weight'),
+    ('by_volume', 'By Volume'),
 ]
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    landed_cost_ok = fields.Boolean(u'Frais dossier import',default=False)
-    split_method = fields.Selection(SPLIT_METHOD, u'Mode répartition',default='by_current_cost_price')
-    is_douane = fields.Boolean(u'Frais douane',default=False)
-    is_copyright = fields.Boolean(u'Droits d\'auteurs',default=False)
-    taux_douane = fields.Float('Taux douane %',size='64',copy=False)
-    copyright = fields.Float(u'Droits d\'auteurs',size='64',copy=False)
+    landed_cost_ok = fields.Boolean(u'Import folder fees',default=False)
+    split_method = fields.Selection(SPLIT_METHOD, u'Dispatch mode',default='by_current_cost_price')
+    is_douane = fields.Boolean(u'Customs fees',default=False)
+    is_copyright = fields.Boolean(u'Copyrights',default=False)
+    taux_douane = fields.Float('Customs rates %',size='64',copy=False)
+    copyright = fields.Float(u'Copyrights',size='64',copy=False)
